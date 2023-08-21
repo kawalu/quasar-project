30206c9065040aaa00c6c7860e7d01543f3c67cb
kawalu <127477124+kawalu@users.noreply.github.com>
2023-08-21 08:04:24 +0000

    Deploying to gh-pages from @ kawalu/quasar-project@3f728ce6f31c95e1e191eea6365ec8b507d32c4e 🚀

--- assets/QCardSection.7aa7476b.js
+++
@@ -1,1 +0,0 @@
-import{j as e,c as r,h as n,E as s}from"./index.189f56a6.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=r(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{i as Q};
