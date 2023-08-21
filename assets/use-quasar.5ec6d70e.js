8809710f123ff4d1a17517376e40a4064ccf02f9
kawalu <127477124+kawalu@users.noreply.github.com>
2023-08-21 06:54:31 +0000

    Deploying to gh-pages from @ kawalu/quasar-project@7926cfcbb3ead3a6c4474aec1d4c757bb203b677 🚀

--- assets/use-quasar.5ec6d70e.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aj as r}from"./index.ce61692c.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar.94f38d34.js
@@ -0,0 +1,1 @@
+import{i as a,aj as r}from"./index.189f56a6.js";function u(){return a(r)}export{u};
