2e2a6840b434826dcf462882edb40f11151d15fd
kawa <david10741@gmail.com>
2023-08-23 16:28:54 +0800

    上傳7

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,7 @@
 import enUS from './en-US'
+import zhTW from './zh-TW'
 
 export default {
+  'zh-TW': zhTW,
   'en-US': enUS
 }
--- src/pages/front/ExploreKawa.vue
+++ src/pages/front/ExploreKawa.vue
@@ -45,7 +45,27 @@
         </div>
       </div>
     </div>
+  </div>
+  <div class="q-pa-none example-column-horizontal-alignment" style="background: #424242; color: #fff;">
+    <div class="row">
+      <div class="col-4 q-pa-xl q-gutter-md" style="margin-left: 150px; ">
+        <q-img src="../../assets/企鵝.png"></q-img>
+      </div>
+      <div class="col-6 q-pa-xl q-gutter-md" style="margin-top: 70px;">
+        <p style="font-size: 16px; font-weight: bolder;">
+          大家好！我是KAWA。未來將由我帶領 KAWA Taiwan 暨全體員工繼續為台灣的消費者服務，持續推動 KAWA 品牌在台灣市場茁壯。為了讓台灣消費者願意繼續支持 KAWA 的產品與服務，我將秉持最能代表 KAWA
+          文化的基本理念，亦即「尊重個人」以及「三種喜悅」。這也是我在整個職業生涯中一直遵循的方針。所謂「尊重個人」就是重視每位顧客、經銷夥伴、協力廠商及 KAWA Taiwan
+          全體員工的想法。並透過「三種喜悅」讓每位顧客、所有相關人員都能「發自內心微笑」。未來也會依循上述理念同時展開溝通交流，期盼可以「讓所有與 KAWA 相關的人們獲得最大滿足」，進而推動 KAWA 在台灣的事業經營。
 
+          汽車產業目前正面臨百年一遇的重大轉型時期，此外全球市場也遭受新冠病毒的影響與打擊。接下來我們將投入更具吸引力的產品，以及提供更加完善的服務，期望在各個領域能夠發揮相乘效果，讓所有顧客滿意，擁有充滿樂趣的用車生活。
+
+          承蒙各位長期的支持與愛護，KAWA Taiwan 在2022年已正式邁入設立20周年的里程碑，感謝從過去至今一路陪伴 KAWA Taiwan 走來的朋友們，未來還請大家繼續支持並陪伴 KAWA Taiwan
+          迎向下一個20周年。
+
+          謝謝！
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
